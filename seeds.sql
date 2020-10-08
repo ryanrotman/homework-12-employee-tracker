604f9ec31c1fc3a87e78d25960ee1e15b485eefe
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-08 15:02:58 -0500

    added seed data for the tables

--- seeds.sql
+++ seeds.sql
@@ -29,3 +29,34 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+INSERT INTO department
+    (name)
+VALUES
+    ("Education"),
+    ("Events"),
+    ("Marketing"),
+    ("Research")
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ("Education Director", 80,000.00, (SELECT id FROM department WHERE name = "Education")),
+    ("Events Director", 80,000.00, (SELECT id FROM department WHERE name = "Events")),
+    ("Marketing Director", 80,000.00, (SELECT id FROM department WHERE name = "Marketing")),
+    ("Research Director", 80,000.00, (SELECT id FROM department WHERE name = "Research")),
+    ("Education Manager", 70,000.00, (SELECT id FROM department WHERE name = "Education")),
+    ("Events Manager", 70,000.00, (SELECT id FROM department WHERE name = "Events")),
+    ("Marketing Manager", 70,000.00, (SELECT id FROM department WHERE name = "Marketing")),
+    ("Research Manager", 70,000.00, (SELECT id FROM department WHERE name = "Research")),
+    ("Education Coordinator", 50,000.00, (SELECT id FROM department WHERE name = "Education")),
+    ("Events Coordinator", 50,000.00, (SELECT id FROM department WHERE name = "Events")),
+    ("Marketing Coordinator", 50,000.00, (SELECT id FROM department WHERE name = "Marketing")),
+    ("Research Coordinator", 50,000.00, (SELECT id FROM department WHERE name = "Research")),
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Dorit", "Lessard", (SELECT id FROM role WHERE title = "Education Director")),
+    ("Ben", "Helt", (SELECT id FROM role WHERE title = "Education Manager"), (SELECT id FROM employee WHERE first_name = "Dorit")),
+    ("Peter", "Giuliano", (SELECT id FROM role WHERE title = "Research Director"))

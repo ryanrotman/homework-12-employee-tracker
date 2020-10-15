d0aceb32d7706cba38434d8635370389f39fe063
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-15 17:20:24 -0500

    DO NOT USE - commented out and will return to for future development to get app working using multiple files

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// Load Requirements
-const mysql = require("mysql");
-const userPrompt = require("./lib/initialPrompt");
-const util = require("util");
+// // Load Requirements
+// const mysql = require("mysql");
+// const userPrompt = require("./lib/initialPrompt");
+// const util = require("util");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "84Vq#sZ8>p",
-  database: "emp_trackerDB"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "84Vq#sZ8>p",
+//   database: "emp_trackerDB"
+// });
 
-connection.query = util.promisify(connection.query);
+// connection.query = util.promisify(connection.query);
 
-connection.connect(err => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Connected as ID '${connection.threadId}'`);
-  userPrompt.begin();
-});
+// connection.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(`Connected to DB with ID '${connection.threadId}'`);
+//   userPrompt.begin();
+// });
 
-module.exports = {connection};
+// module.exports = {connection};
